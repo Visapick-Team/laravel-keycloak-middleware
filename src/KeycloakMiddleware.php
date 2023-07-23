0fe772a9beb78615730dc6693615250bb1c83a79
Alireza Salehi <46676671+SalehiAlireza@users.noreply.github.com>
2023-07-23 12:09:28 +0330

    add support scope and role

--- src/KeycloakMiddleware.php
+++ src/KeycloakMiddleware.php
@@ -3,6 +3,7 @@
 namespace Pickmap\Keycloak;
 
 use Closure;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response as HttpResponse;
 use Illuminate\Support\Facades\Validator;
@@ -15,8 +16,8 @@ class KeycloakMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
-    {    
+    public function handle(Request $request, Closure $next,$scopes = '*',$roles = '*'): Response
+    {            
         $validator = Validator::make(
             ['token' => $request->bearerToken()],
             [
@@ -36,6 +37,56 @@ class KeycloakMiddleware
                 ] , HttpResponse::HTTP_UNAUTHORIZED
             );
         }
+
+        // You do not have access
+        if ($this->accessDenied($request,$scopes,$roles)) 
+        {
+            return Response()->json(
+                [
+                'has_error' => true,
+                'errors' => 'Access Denied'
+                ] , HttpResponse::HTTP_FORBIDDEN
+            );
+        }
+
+
+
         return $next($request);
+    }
+
+    public function accessDenied($request,$scopes,$roles) :bool
+    {
+        // from developer assingn
+        $routeScopes     = explode('|',$scopes);
+        $routeRoles      = explode('|',$roles);
+
+        // from keycloak
+        $tokenData       = $request->all()['token_data'];
+        $tokenScope      = explode(' ',$tokenData['scope']) ;
+        $tokenRoles      = $tokenData['realm_access']->roles;      
+        
+        $response = $this->checkExists($routeScopes,$tokenScope) && $this->checkExists($routeRoles,$tokenRoles);
+        return ! $response;
+    }
+
+    // Check for the presence of specified scope nd role in the route in the token
+    public function checkExists($scopeOrRolesInRoute,array $scopeOrRolesInToken):bool
+    {
+        // the meaning * is global scope or route
+        $hasAccess   = in_array('*',$scopeOrRolesInRoute);
+
+        if (! $hasAccess)
+        {
+            // check definde scope or role in route exists in token data
+            foreach ($scopeOrRolesInRoute as $scopeOrRolesItem) 
+            {                
+                if (in_array($scopeOrRolesItem,$scopeOrRolesInToken))
+                {
+                    $hasAccess = true;
+                }
+            }
+        }
+
+        return $hasAccess;
     }
 }
